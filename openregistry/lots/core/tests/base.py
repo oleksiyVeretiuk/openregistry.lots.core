3f0d9a732b12e0036903c7501e8e4e524fb141a4
sasha-kantoriz <sasha-kantoriz@quintagroup.org>
2017-08-19 16:06:24 +0300

    Inherit BaseLotWebTest from BaseResourceWebTest

--- openregistry/lots/core/tests/base.py
+++ openregistry/lots/core/tests/base.py
@@ -9,7 +9,7 @@ from datetime import datetime
 from requests.models import Response
 from webtest import TestApp
 
-from openregistry.api.tests.base import BaseWebTest
+from openregistry.api.tests.base import BaseWebTest, BaseResourceWebTest
 from openregistry.api.utils import apply_data_patch
 from openregistry.api.constants import SESSION
 
@@ -17,97 +17,12 @@ from openregistry.api.constants import SESSION
 now = datetime.now()
 
 
-class BaseLotWebTest(BaseWebTest):
+class BaseLotWebTest(BaseResourceWebTest):
+
+    resource_name = 'lots'
     initial_data = None
     initial_status = None
-    initial_bids = None
-    initial_lots = None
     docservice = False
-    relative_to = os.path.dirname(__file__)
-
-    def set_status(self, status, extra=None):
-        data = {'status': status}
-        if status == "pending":
-            data['status'] = status
-
-        if extra:
-            data.update(extra)
-
-        lot = self.db.get(self.lot_id)
-        lot.update(apply_data_patch(lot, data))
-        self.db.save(lot)
-
-        response = self.app.get('/lots/{}'.format(self.lot_id))
-        self.assertEqual(response.status, '200 OK')
-        self.assertEqual(response.content_type, 'application/json')
-        lot = response.json['data']
-        self.assertEqual(lot['status'], status)
-        return lot
-
-    def create_lot(self, extra=None):
-        data = deepcopy(self.initial_data)
-        if extra:
-            data.update(extra)
-        response = self.app.post_json('/lots', {'data': data})
-        lot = response.json['data']
-        self.lot_token = response.json['access']['token']
-        self.lot_id = lot['id']
-        status = lot['status']
-        if self.initial_status != status:
-            lot = self.set_status(self.initial_status)
-        return lot
-
-    def setUp(self):
-        super(BaseLotWebTest, self).setUp()
-        if self.docservice:
-            self.setUpDS()
-
-    def setUpDS(self):
-        self.app.app.registry.docservice_url = 'http://localhost'
-        test = self
-        def request(method, url, **kwargs):
-            response = Response()
-            if method == 'POST' and '/upload' in url:
-                url = test.generate_docservice_url()
-                response.status_code = 200
-                response.encoding = 'application/json'
-                response._content = '{{"data":{{"url":"{url}","hash":"md5:{md5}","format":"application/msword","title":"name.doc"}},"get_url":"{url}"}}'.format(url=url, md5='0'*32)
-                response.reason = '200 OK'
-            return response
-
-        self._srequest = SESSION.request
-        SESSION.request = request
-
-    def setUpBadDS(self):
-        self.app.app.registry.docservice_url = 'http://localhost'
-        def request(method, url, **kwargs):
-            response = Response()
-            response.status_code = 403
-            response.encoding = 'application/json'
-            response._content = '"Unauthorized: upload_view failed permission check"'
-            response.reason = '403 Forbidden'
-            return response
-
-        self._srequest = SESSION.request
-        SESSION.request = request
-
-    def generate_docservice_url(self):
-        uuid = uuid4().hex
-        key = self.app.app.registry.docservice_key
-        keyid = key.hex_vk()[:8]
-        signature = b64encode(key.signature("{}\0{}".format(uuid, '0' * 32)))
-        query = {'Signature': signature, 'KeyID': keyid}
-        return "http://localhost/get/{}?{}".format(uuid, urlencode(query))
-
-    def tearDownDS(self):
-        SESSION.request = self._srequest
-
-    def tearDown(self):
-        if self.docservice:
-            self.tearDownDS()
-        if hasattr(self, 'lot_id'):
-            del self.db[self.lot_id]
-        super(BaseLotWebTest, self).tearDown()
 
 
 class DumpsTestAppwebtest(TestApp):
