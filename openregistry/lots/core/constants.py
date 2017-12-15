fe93ee5b6bda1c466afa40d9115c02e1fa288a05
Oleksiy Veretiuk <oleksiy.veretiuk@gmail.com>
2017-12-15 19:04:57 +0200

    Add recomposed status

--- openregistry/lots/core/constants.py
+++ openregistry/lots/core/constants.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 DEFAULT_LOT_TYPE = 'basic'
 
-LOT_STATUSES = ["draft", "pending", "deleted", "verification",
+LOT_STATUSES = ["draft", "pending", "deleted", "verification", "recomposed",
                 "active.salable", "pending.dissolution", "dissolved", "active.awaiting",
                 "active.auction", "sold"]
