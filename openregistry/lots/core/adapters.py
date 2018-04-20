10c1def3f6e2088a718e1ab5633628d1205375d9
Oleksiy Veretiuk <oleksiy.veretiuk@gmail.com>
2018-04-20 14:25:35 +0300

    Add LotManagerAdapter

--- openregistry/lots/core/adapters.py
+++ openregistry/lots/core/adapters.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 from openprocurement.api.adapters import ContentConfigurator
+from openprocurement.api.utils import error_handler
 
 
 class LotConfigurator(ContentConfigurator):
@@ -7,3 +8,19 @@ class LotConfigurator(ContentConfigurator):
 
     name = "Lot Configurator"
     model = None
+
+
+class LotManagerAdapter(object):
+    name = 'Lot Manager'
+    context = None
+
+    def __init__(self, context):
+        self.context = context
+
+    def _validate(self, request, validators):
+        kwargs = {'request': request, 'error_handler': error_handler}
+        for validator in validators:
+            validator(**kwargs)
+
+    def create_lot(self, request):
+        pass
