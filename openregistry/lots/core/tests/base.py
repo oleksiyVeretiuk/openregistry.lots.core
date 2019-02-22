610cddaacbe915f8df9d0c33dcfba23b7f22417e
Bohdan Dmytriv <7142689+bdmbdsm@users.noreply.github.com>
2019-02-22 16:57:45 +0200

    MigrationResourcesDTO_mock import forward (#75)
    * MigrationResourcesDTO_mock import forward
    
    * Comment update

--- openregistry/lots/core/tests/base.py
+++ openregistry/lots/core/tests/base.py
@@ -17,8 +17,9 @@ from openprocurement.api.tests.base import MOCK_CONFIG as BASE_MOCK_CONFIG
 from openprocurement.api.tests.blanks.related_processes import (
     RelatedProcessesTestMixinBase,  # forwarded import
 )
-from openregistry.lots.core.tests.fixtures import PARTIAL_MOCK_CONFIG
+from openprocurement.api.tests.fixtures.mocks import MigrationResourcesDTO_mock  # noqa import fowrard
 from openprocurement.api.utils import connection_mock_config
+from openregistry.lots.core.tests.fixtures import PARTIAL_MOCK_CONFIG
 
 MOCK_CONFIG = connection_mock_config(PARTIAL_MOCK_CONFIG, ('plugins',), BASE_MOCK_CONFIG)
 
